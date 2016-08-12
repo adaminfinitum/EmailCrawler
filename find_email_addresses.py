070fd2a835576b68f9cfb757bde333860c17c68d
Nicholas Hinsch <nicholas@evalagency.com>
2016-08-12 02:27:13 -0400

    Huzzah! first commit :p

--- find_email_addresses.py
+++ find_email_addresses.py
@@ -1,57 +1,241 @@
-from bs4 import BeautifulSoup, SoupStrainer
+#!/usr/bin/env python
+#
+# requires python version 3 or greater.
+#
+# @copyright 2016 Nicholas Hinsch, based on work by Arun-UB https://github.com/Arun-UB
+#
+# This script supports extracting multiple email addresses in mailto links from multiple webpages on any number of supplied domains.
+# The page crawler is somewhat intelligent (it will skip assets like images, videos and documents as well as skipping offsite links.
+# It also normalizes links that don't have the FQDN in the href.
+#
+# This doesn't parse javascript, so obfuscated emails are not detected. If I get any more spare time, I will see about adding that support.
+# You can also set a crawl rate (delay) and maximum number of pages.
+#
+# Duplicate emails per domain are also stripped out. You're welcome :p
+# sample use:
+# python3 find_email_addresses.py --domains www.rapidtables.com/web/html/mailto.htm https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/Email_links --delay 1 --maxpages 2
+#
+# Checking http://www.rapidtables.com/web/html/mailto.htm for email addresses.
+# The following emails were found:
+# name3@rapidtables.com
+# name1@rapidtables.com
+# name@rapidtables.com
+# name2@rapidtables.com
+# Checking https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/Email_links for email addresses.
+# The following emails were found:
+# nobody@mozilla.org
+# nowhere@mozilla.org
+#
+# If you run into problems, enable debug mode by supplying --verbose on the command line
+#
+
+from bs4 import BeautifulSoup
 import urllib
-import urlparse
 import argparse
 import re
-import Queue
+import queue
+import time
 
 class Crawler(object):
-	def __init__(self, domainName):
-		self.domainName = domainName
-		self.url = domainName
-		self.emails = []
-		
-		if not re.match(r'http(s?)\:www', domainName):
-			self.url = 'http://www.' + domainName
+    def __init__(self, url, delay, maxpages, verbose):
 
-	def get_emails(self):
-		return list(set(self.emails))
+        self.delay = delay
+        self.maxpages = maxpages
+        self.verbose = verbose
 
-	def extract_emails(self,page):
-		[self.emails.append(link.get('href')[7:]) for link in
-				page.select('a[href^=mailto]')]
-		
-	def get_links(self,page):
-		links = {}
-		links = {link.get('href') for link in page.find_all('a') 
-							if self.domainName in str(link.get('href'))}
-		return list(links)
+        # normalize the supplied url protocol
+        if not re.match('https?://|www\\\.', url):
+            url = 'http://' + url
 
-	def crawl(self):
+        # Strip off query string params on url
+        self.url = urllib.parse.urljoin(url, urllib.parse.urlparse(url).path)
 
-		tocrawl = Queue.Queue()
-		crawled = []
-		tocrawl.put(self.url)
+        # extract the domain name from the url
+        self.domainName = urllib.parse.urlparse(url).netloc
 
-		while tocrawl.qsize():
-			link = str(tocrawl.get())
-			#To remove query string params
-			link = urlparse.urljoin(link, urlparse.urlparse(link).path)
+        self.emails = []
 
-			page = BeautifulSoup(urllib.urlopen(link).read(),'html.parser')			# print(tocrawl.qsize())
-			crawled.append(link)
-			[tocrawl.put(l) for l in self.get_links(page) if l not in crawled]
-			self.extract_emails(page)
+    def get_emails(self):
+        return list(set(self.emails))
 
+    def extract_emails(self, page):
+        for link in page.select('a[href^=mailto]'):
 
-if __name__=="__main__":
-	parser = argparse.ArgumentParser(description="Enter a URL")
-	parser.add_argument('domainName')
-	args = parser.parse_args()
-	c = Crawler(args.domainName.strip())
-	c.crawl()
-	print("The following emails were found")
-	for email in c.get_emails():
-		print(email)
+            # split apart multi-recipient mailto links
+            emailaddresses = link.get('href')[7:].split(',')
+
+            for addy in emailaddresses:
+                #extract any additional recipients, cc's and bcc's
+                pattern = re.compile("[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+")
+
+                all_recipients = pattern.findall(addy)
+
+                # this additional loop also removes duplicates, albeit in a not so elegant way
+                for i in all_recipients:
+                    self.emails.append(i)
 
 
+    def get_links(self, page):
+        links = []
+
+        for link in page.find_all('a'):
+            if link.get('href') is None:
+                self.debug('skipping homepage link!')
+
+            elif link.get('href').startswith('#'):
+                self.debug('skipping anchor!')
+
+            elif link.get('href').startswith('//'):
+                self.debug('skipping external or protocol relative link!')
+
+            elif link.get('href').startswith('/'):
+                link = urllib.parse.urljoin(self.url, link.get('href'))
+                links.append(link)
+
+            elif self.domainName not in link.get('href'):
+                self.debug('skipping external link!')
+
+            else:
+                link = urllib.parse.urljoin(self.url, link.get('href'))
+                links.append(link)
+
+        self.debug('found the following url links:')
+        self.debug(links)
+
+        return list(links)
+
+    def crawl(self):
+        pagecount = 0
+        excludedExtensions = (
+            '.jpg', '.jpeg', '.png', '.gif',
+            '.tif', '.doc', '.docx', '.xls',
+            '.xlsx', '.pdf', '.log', '.msg',
+            '.odt', '.pages', '.rtf', '.tex',
+            '.wpd', '.wps', '.csv', '.ppt',
+            '.pptx', '.zip', '.tar', '.xml',
+            '.bz', '.tgz', '.tar.gz', '.vcf',
+            '.aif', '.m3u', '.m4a', '.mid',
+            '.mp3', '.mpa', '.wav', '.wma',
+            '.avi', '.flv', '.mpg', '.mov',
+            '.m4v', '.mp4', '.rm', '.swf',
+            '.wmv', '.obs', '.3dm', '.3ds',
+            '.max', '.bmp', '.psd', '.ai',
+            '.tiff', '.eps', '.ps', '.svg',
+            '.indd', '.pct', '.xlr', '.db',
+            '.sql', '.dbf', '.pdb', '.app',
+            '.bat', '.jar', '.wsf', '.rom',
+            '.sav', '.dwg', '.dxf', '.kmz',
+            '.ini', '.cfg', '.7z', '.cbr',
+            '.rar', '.pkg', '.sitx', '.zipx',
+            '.bin', '.cue', '.dmg', '.iso',
+            '.mdf', '.toast', '.vcd', '.c',
+            '.py', '.cpp', '.class', '.java',
+            '.pl', '.sh', '.vb', '.swift',
+            '.bak', '.tmp', '.ics', '.exe',
+            '.msi', '.torrent', '.lua', '.deb',
+            '.rpm', '.hqx', '.uue', '.sys'
+        )
+        tocrawl = queue.Queue()
+        crawled = []
+        tocrawl.put(self.url)
+
+        while tocrawl.qsize():
+
+            if pagecount == self.maxpages:
+                break
+
+            link = str(tocrawl.get())
+
+            if link.lower().endswith(excludedExtensions):
+                self.debug('Skipping : ' + link + ' because it\'s an asset, not a page!')
+
+            elif link not in crawled:
+                self.debug('Found a new page to crawl: ' + link)
+
+                # skip dead links and pages we've already crawled
+                try:
+                    pageContent = urllib.request.urlopen(link, None)
+                    page = BeautifulSoup(pageContent, 'html.parser')
+
+                    crawled.append(link)
+
+                    # queue up new links found on this page
+                    for l in self.get_links(page):
+                        if l not in crawled:
+                            tocrawl.put(l)
+                            self.debug('-- Adding link: ' + l + ' found on page')
+
+                    self.extract_emails(page)
+
+                    pagecount += 1
+
+                except urllib.error.HTTPError as e:
+                    if e.code == 403:
+                        self.debug('Skipping page: ' + link + ' because the crawler was forbidden access (403)!')
+                    elif e.code == 404:
+                        self.debug('Skipping page: ' + link + ' because the link is dead (404)!')
+                    else:
+                        self.debug('Skipping page: ' + link + 'because something else went wrong.  HTTP error code: ' + e.code)
+
+                except urllib.error.URLError as e:
+                    self.debug('Skipping unknown URL type!')
+
+                    crawled.append(link)
+
+                # wait for the specified amount of time between page requests...be nice to webservers!
+                time.sleep(self.delay)
+
+            else:
+                self.debug('Skipping page: ' + link + ' because we\'ve already crawled this page!')
+
+    # Log all the things!
+    def debug(self, msg, prefix='DEBUG'):
+        if self.verbose:
+            print(prefix + ' ' + str(msg))
+
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Enter a URL")
+
+    parser.add_argument(
+        '--domains', nargs='+',
+        help='Space-separated list of domains to crawl'
+    )
+
+    parser.add_argument(
+        '--delay', nargs='?',
+        default=5,
+        help='The delay between page requests in seconds (default is 5 seconds)'
+    )
+
+    parser.add_argument(
+        '--maxpages', nargs='?',
+        default=5,
+        help='The maximum number of pages to crawl per domain (default is 100 pages)'
+    )
+
+    parser.add_argument(
+        '--verbose',
+        action='store_true',
+        help='Enable verbose mode. This prints a fuck-load of debug info to stdout (your terminal).'
+    )
+
+    args = parser.parse_args()
+
+    domains = args.domains
+    delay = int(args.delay)
+    maxpages = int(args.maxpages)
+    verbose = args.verbose
+
+    for domain in domains:
+
+        c = Crawler(str(domain).strip(), delay, maxpages, verbose)
+
+        print("Checking " + c.url + " for email addresses.")
+        c.crawl()
+
+        print("The following emails were found:")
+        for email in c.get_emails():
+            print(email)
